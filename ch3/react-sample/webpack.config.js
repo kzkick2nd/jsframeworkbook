7bddaaa30b2e4eb88901af37a15a65784a29dc42
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-31 20:02:18 +0900

    Set Loader

--- ch3/react-sample/webpack.config.js
+++ ch3/react-sample/webpack.config.js
@@ -5,5 +5,26 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_module/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env', '@babel/react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.css']
+    },
 };
